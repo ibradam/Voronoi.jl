660aac5af5179604bb1626e9c3a4295ff0f7d66a
Mourrain Bernard <bernard.mourrain@inria.fr>
2018-02-13 14:24:29 +0100

    hline + test

--- src/hline.jl
+++ src/hline.jl
@@ -1,4 +1,26 @@
-struct hline
+mutable struct HLine
 
+    m_pt  ::Vector{Float64}
+    m_dir ::Vector{Float64}
 
 end    
+
+function hline()
+    HLine([0.0,0.0,0.0], [0.0,0.0,1.0] )
+end
+
+function hline(p::Vector{Float64})
+    HLine(p, [0.0,0.0,1.0] )
+end
+
+function hline(p::Vector{Float64}, d::Vector{Float64})
+    HLine(p, d)
+end
+
+function point(L::HLine)
+    return L.m_pt
+end
+
+function dir(L::HLine)
+    return L.m_dir
+end
