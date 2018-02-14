c03d937e85186185d6e8e37392252d5351223370
Ibrahim Adamou <adamouhakime14@gmail.com>
2018-02-14 10:19:40 +0100

    distance

--- src/distance.jl
+++ src/distance.jl
@@ -3,11 +3,14 @@
 #  parameters: p,H 
 #  the squared euclidean distance from the half-line H to a point p.
 #
-function distance2(H,p)
- u = p - m_pt
- s = dot(m_dir, u)
- if s<0
+function distance2(H::HLine,p::Vector{Float64})
+ u = p - H.m_pt
+ s = dot(H.m_dir, u)
+  if s<0
     return norm(u)^2
-      else
-        return norm(u)^2-s^2
+  else
+    return norm(u)^2-s^2
+  end
 end
+
+
